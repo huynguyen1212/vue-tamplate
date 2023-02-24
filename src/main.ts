c15b686066dbdd0f3104afdac356ebdbbab3e22d
huynguyen1212 <huyro1212000@gmail.com>
2023-02-24 10:05:47 +0700

    config commit git

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "../src/routes";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from '../src/routes'
+import { createPinia } from 'pinia'
 
-import "ant-design-vue/dist/antd.css";
+import 'ant-design-vue/dist/antd.css'
 
-const app = createApp(App);
-app.use(router);
-app.use(createPinia());
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
 
-app.mount("#app");
+app.mount('#app')
